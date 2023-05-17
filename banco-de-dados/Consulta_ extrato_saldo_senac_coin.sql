fa03bfcdf3631340dd7e3c3222753b7b92fe0191
Alexandre Trennephol <110431351+tr4nnp0@users.noreply.github.com>
2023-05-17 16:25:08 -0300

    Delete banco-de-dados directory

--- banco-de-dados/Consulta_ extrato_saldo_senac_coin.sql
+++
@@ -1,8 +0,0 @@
--- Consulta ao banco de dados do SENAC, solicitada como atividade de avaliação na disciplina de projeto integrador,
--- com o objetivo de gerar o extrato do usuário em senac coins, moedas obtidas através da entrega de tarefas que podem
--- ser trocadas por produtos e serviços.
-
-SELECT senac_coin.senac_coin_id, senac_coin_movimentacao.senac_coin_movimentacao_data,
-senac_coin_movimentacao.senac_coin_movimentacao_valor AS extrato_movimentacao
-FROM bdacompanhamento.senac_coin_movimentacao
-WHERE senac_coin.senac_coin_id = 1;
