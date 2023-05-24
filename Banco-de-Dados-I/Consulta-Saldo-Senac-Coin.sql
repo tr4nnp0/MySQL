d46c556413251ef479167c052ed837ecdcd394e1
Alexandre Trennephol <110431351+tr4nnp0@users.noreply.github.com>
2023-05-24 01:24:04 -0300

    Delete Consulta-Saldo-Senac-Coin.sql

--- Banco-de-Dados-I/Consulta-Saldo-Senac-Coin.sql
+++
@@ -1,8 +0,0 @@
--- Consulta ao banco de dados do SENAC, solicitada como atividade de avaliação na disciplina de projeto integrador,
--- com o objetivo de gerar o saldo do usuário em senac coins, moedas obtidas através da entrega de tarefas que podem
--- ser trocadas por produtos e serviços.
-
-SELECT senac_coin.senac_coin_id, senac_coin_movimentacao.senac_coin_movimentacao_data,
-senac_coin.senac_coin_saldo + senac_coin_movimentacao.senac_coin_movimentacao_valor AS saldo_senac_coin
-FROM bdacompanhamento.senac_coin, bdacompanhamento.senac_coin_movimentacao
-WHERE senac_coin.senac_coin_id = 1;
